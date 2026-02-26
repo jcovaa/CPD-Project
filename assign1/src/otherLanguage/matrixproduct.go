3b59b8e48e42e7dc0ac23f0139b8ed2c5a06a390
jcovaa <joao.vilacova281205@gmail.com>
2026-02-26 16:25:03 +0000

    added onMult function for go

--- assign1/src/otherLanguage/matrixproduct.go
+++ assign1/src/otherLanguage/matrixproduct.go
@@ -2,10 +2,58 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func OnMult(m_ar int, m_br int) {
-	//var st [100]char;
+	// Variable declaration
+	var temp float64
+
+	// Creating matrix
+	// make() allocates and initializes memory
+	pha := make([]float64, m_ar*m_ar)
+	phb := make([]float64, m_ar*m_ar)
+	phc := make([]float64, m_ar*m_ar)
+
+	// Initializing the pha matrix with 1's
+	for i := 0; i < m_ar; i++ {
+		for j := 0; j < m_ar; j++ {
+			pha[i*m_ar+j] = 1.0
+		}
+	}
+
+	// Initializing the phb matrix
+	for i := 0; i < m_br; i++ {
+		for j := 0; j < m_br; j++ {
+			phb[i*m_br+j] = float64(i + 1)
+		}
+	}
+
+	// Starting clock
+	start := time.Now()
+
+	// Calculating the multiplication
+	for i := 0; i < m_ar; i++ {
+		for j := 0; j < m_br; j++ {
+			temp = 0
+			for k := 0; k < m_ar; k++ {
+				temp += pha[i*m_ar+k] * phb[k*m_br+j]
+			}
+			phc[i*m_ar+j] = temp
+		}
+	}
+
+	// Calculate time
+	duration := time.Since(start)
+	fmt.Println("Time: ", duration)
+
+	fmt.Println("Result matrix: ")
+	for i := 0; i < 1; i++ {
+		for j := 0; j < min(10, m_br); j++ {
+			fmt.Print(phc[j], " ")
+		}
+	}
+	fmt.Println()
 }
 
 func OnMultLine(m_ar int, m_br int) {
