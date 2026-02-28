2726f5b05ba8d512b9d983f341ea2110d5d05929
jcovaa <joao.vilacova281205@gmail.com>
2026-02-28 14:45:36 +0000

    added second algorithm

--- assign1/src/matrixproduct.go
+++ assign1/src/matrixproduct.go
@@ -57,7 +57,53 @@ func OnMult(m_ar int, m_br int) {
 }
 
 func OnMultLine(m_ar int, m_br int) {
+	// Variable declaration
+	var temp float64
 
+	// Creating matrix
+	// make() allocates and initializes memory
+	pha := make([]float64, m_ar*m_ar)
+	phb := make([]float64, m_ar*m_ar)
+	phc := make([]float64, m_ar*m_ar)
+
+	// Initializing the pha matrix with 1's
+	for i := 0; i < m_ar; i++ {
+		for j := 0; j < m_ar; j++ {
+			pha[i*m_ar+j] = 1.0
+		}
+	}
+
+	// Initializing the phb matrix
+	for i := 0; i < m_br; i++ {
+		for j := 0; j < m_br; j++ {
+			phb[i*m_br+j] = float64(i + 1)
+		}
+	}
+
+	// Starting clock
+	start := time.Now()
+
+	// Calculating the multiplication
+	for i := 0; i < m_ar; i++ {
+		for k := 0; k < m_ar; k++ {
+			temp = pha[i*m_ar+k]
+			for j := 0; j < m_br; j++ {
+				phc[i*m_ar+j] += temp * phb[k*m_br+j]
+			}
+		}
+	}
+
+	// Calculate time
+	duration := time.Since(start).Seconds()
+	fmt.Printf("Time: %3.3f seconds\n", duration)
+
+	fmt.Println("Result matrix: ")
+	for i := 0; i < 1; i++ {
+		for j := 0; j < min(10, m_br); j++ {
+			fmt.Print(phc[j], " ")
+		}
+	}
+	fmt.Println()
 }
 
 func main() {
