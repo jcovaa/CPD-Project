2d2bc813f76cfe0a2003528524ae9987e6a50e9a
Pedro Castro <falecompedroac@gmail.com>
2026-03-12 11:19:04 +0000

    first algorithm parallel version 1

--- assign1/src/matrixproduct.cpp
+++ assign1/src/matrixproduct.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include <time.h>
 #include <cstdlib>
+#include <omp.h>
+#include <chrono>
 
 using namespace std;
 
@@ -48,6 +50,63 @@ void OnMult(int m_ar, int m_br)
    Time2 = clock();
    snprintf(st, sizeof(st), "Time: %3.3f seconds\n",
             (double)(Time2 - Time1) / CLOCKS_PER_SEC);
+   cout << st;
+
+   cout << "Result matrix: " << endl;
+   for (i = 0; i < 1; i++)
+   {
+      for (j = 0; j < min(10, m_br); j++)
+         cout << phc[j] << " ";
+   }
+   cout << endl;
+
+   free(pha);
+   free(phb);
+   free(phc);
+}
+
+void OnMultParallel1(int m_ar, int m_br)
+{
+   typedef chrono::high_resolution_clock clock;
+   chrono::time_point<clock> Time1, Time2;
+
+   char st[100];
+   double temp;
+   int i, j, k;
+
+   double *pha, *phb, *phc;
+
+   pha = (double *)malloc((m_ar * m_ar) * sizeof(double));
+   phb = (double *)malloc((m_ar * m_ar) * sizeof(double));
+   phc = (double *)malloc((m_ar * m_ar) * sizeof(double));
+
+   for (i = 0; i < m_ar; i++)
+      for (j = 0; j < m_ar; j++)
+         pha[i * m_ar + j] = 1.0;
+
+   for (i = 0; i < m_br; i++)
+      for (j = 0; j < m_br; j++)
+         phb[i * m_br + j] = (double)(i + 1);
+
+   Time1 = clock::now();
+
+#pragma omp parallel for
+   for (i = 0; i < m_ar; i++)
+   {
+      for (j = 0; j < m_br; j++)
+      {
+         temp = 0;
+         for (k = 0; k < m_ar; k++)
+         {
+            temp += pha[i * m_ar + k] * phb[k * m_br + j];
+         }
+         phc[i * m_ar + j] = temp;
+      }
+   }
+
+   Time2 = clock::now();
+   snprintf(st, sizeof(st), "Time: %3.3f seconds\n",
+            chrono::duration<double>(Time2 - Time1).count());
    cout << st;
 
    cout << "Result matrix: " << endl;
@@ -210,7 +269,7 @@ int main(int argc, char *argv[])
       switch (op)
       {
       case 1:
-         OnMult(lin, col);
+         OnMultParallel1(lin, col);
          break;
       case 2:
          OnMultLine(lin, col);
