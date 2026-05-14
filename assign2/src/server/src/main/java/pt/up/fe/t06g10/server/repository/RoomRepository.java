334c08f5ef3ea50c86cf43fcf1722991a15feac4
Pedro Castro <falecompedroac@gmail.com>
2026-05-14 15:57:10 +0100

    created missing repositories

---
+++ assign2/src/server/src/main/java/pt/up/fe/t06g10/server/repository/RoomRepository.java
@@ -0,0 +1,46 @@
+package pt.up.fe.t06g10.server.repository;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import pt.up.fe.t06g10.server.database.EntityManagerFactoryProvider;
+import pt.up.fe.t06g10.server.entity.RoomEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public class RoomRepository {
+    public Optional<RoomEntity> findByName(String name) {
+        try (Session session = EntityManagerFactoryProvider.openSession()) {
+            RoomEntity room = session.createQuery("from RoomEntity where name = :name", RoomEntity.class).setParameter("name", name).uniqueResult();
+            return Optional.ofNullable(room);
+        }
+    }
+
+    public List<RoomEntity> findAll() {
+        try (Session session = EntityManagerFactoryProvider.openSession()) {
+            return session.createQuery("from RoomEntity", RoomEntity.class).list();
+        }
+    }
+
+    public boolean existsByName(String name) {
+        try (Session session = EntityManagerFactoryProvider.openSession()) {
+            Long count = session.createQuery("select count(r) from RoomEntity r where r.name = :name", Long.class).setParameter("name", name).uniqueResult();
+            return count != null && count > 0;
+        }
+    }
+
+    public RoomEntity save(RoomEntity room) {
+        Transaction transaction = null;
+        try (Session session = EntityManagerFactoryProvider.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(room);
+            transaction.commit();
+            return room;
+        } catch (RuntimeException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
+    }
+}
