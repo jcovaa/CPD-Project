53e121e4062e2ccc13348380f217151c2d1fbab0
Pedro Castro <falecompedroac@gmail.com>
2026-05-29 11:26:44 +0100

    moved AiService instantiation to getServer method

--- assign2/src/server/src/main/java/pt/up/fe/t06g10/server/Main.java
+++ assign2/src/server/src/main/java/pt/up/fe/t06g10/server/Main.java
@@ -33,12 +33,11 @@ public class Main {
         EntityManagerFactoryProvider.initialize(UserEntity.class, RoomEntity.class, RoomMemberEntity.class, MessageEntity.class);
         Runtime.getRuntime().addShutdownHook(new Thread(EntityManagerFactoryProvider::close));
 
-        AiService aiService = new AiService();
-        ChatServer server = getServer(port, aiService);
+        ChatServer server = getServer(port);
         server.start();
     }
 
-    private static ChatServer getServer(int port, AiService aiService) {
+    private static ChatServer getServer(int port) {
         TokenService tokenService = new TokenService();
         UserRepository userRepository = new UserRepository();
         AuthService authService = new AuthService(userRepository, tokenService);
@@ -46,6 +45,7 @@ public class Main {
         RoomMemberRepository roomMemberRepository = new RoomMemberRepository();
         MessageRepository messageRepository = new MessageRepository();
         SessionManager sessionManager = new SessionManager();
+        AiService aiService = new AiService();
         RoomManager roomManager = new RoomManager(roomRepository, roomMemberRepository, messageRepository, userRepository, sessionManager, aiService);
 
         return new ChatServer(port, authService, tokenService, sessionManager, roomManager);
